d4520475c767545b5b1bbb719ca05798b861491c
Bhadri Narayanan <Bhadri1224>
2026-06-03 17:18:01 +0530

    Updated Schema

--- app/schemas.py
+++ app/schemas.py
@@ -7,3 +7,9 @@ class ProjectCreate(BaseModel):
     project_name: str
     description: str
     summary: str | None = None
+class UserCreate(BaseModel):
+    username: str
+    password: str
+class LoginRequest(BaseModel):
+    username: str
+    password: str
